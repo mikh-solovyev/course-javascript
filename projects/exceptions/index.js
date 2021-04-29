5b92d644903c0fce46cdd150939c59f1dd2a55a7
Mikh <support@web-comp.ru>
2021-04-29 12:26:14 +0300

    Правки по 2-2 ДЗ

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -17,7 +17,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || !array.length) throw new Error('empty array');
+  if (!(array instanceof Array) || !array.length) throw new Error('empty array');
 
   if (typeof fn !== 'function') throw new Error('fn is not a function');
 
@@ -45,7 +45,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || !array.length) throw new Error('empty array');
+  if (!(array instanceof Array) || !array.length) throw new Error('empty array');
 
   if (typeof fn !== 'function') throw new Error('fn is not a function');
 
