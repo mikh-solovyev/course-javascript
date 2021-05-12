44847c6a6df07e7a35aae80eea49244130e3b7f5
Mikhail Solovyev <mikh.develop@gmail.com>
2021-05-12 22:51:19 +0300

    ДЗ 4 недели 1

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,67 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
+let isDrag = false,
+  currentDrag,
+  shiftX,
+  shiftY;
+
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+document.addEventListener('dragstart', (e) => {
+  currentDrag = e.target;
+  e.dataTransfer.setData('text/html', '...');
+  isDrag = true;
+  console.log(isDrag);
+  // Позиция захвата элемента
+  shiftX = e.layerX;
+  shiftY = e.layerY;
+});
+
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  if (isDrag) {
+    currentDrag.style.top = e.pageY - shiftY + 'px';
+    currentDrag.style.left = e.pageX - shiftX + 'px';
+  }
+});
+
+document.addEventListener('drop', (e) => {
+  isDrag = false;
+});
+
+function randInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+export function createDiv() {
+  const div = document.createElement('DIV');
+  div.classList.add('draggable-div');
+  div.draggable = true;
+
+  // Рандом ширина, высота, радиус
+  const width = randInterval(50, 200) + 'px',
+    height = randInterval(50, 200) + 'px',
+    radius = randInterval(0, 50) + '%';
+
+  div.style.width = width;
+  div.style.height = height;
+  div.style.borderRadius = radius;
+
+  // Рандом цвет
+  div.style.background = randColor();
+
+  // Рандом позиция на экране
+  div.style.top = randInterval(1, 100) + '%';
+  div.style.left = randInterval(1, 100) + '%';
+
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
